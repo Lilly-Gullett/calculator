3b3e361008d031bc26151549693b3b8e0d49ded8
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-09-17 09:47:52 -0600

    Connect index.html elements to javascript.js
    This commit connects the buttons from the html file to the javascript.js
    file.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
+let firstNumber;
+let operator;
+let secondNumber;
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+
 function add(array) {
     function getSum(total, num) {
         return total + num;
