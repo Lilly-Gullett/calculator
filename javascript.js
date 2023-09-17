78e9e7748d9cef871df12ad6c07547e0a5c869c5
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-09-17 10:03:48 -0600

    Add operate function to javascript.js file
    This commit adds an operate function to the javascript.js file. This
    function will be used when the equals button is clicked.

--- javascript.js
+++ javascript.js
@@ -12,6 +12,20 @@ const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 const zero = document.querySelector('.zero');
 
+function operate(operator, firstNumber, secondNumber) {
+    firstNumber = parseInt(firstNumber.join(''));
+    secondNumber = parseInt(secondNumber.join('')); //turns the arrays of numbers into each being a single numeric response
+    if (operator === 'add'){
+        return add([firstNumber, secondNumber]);
+    } else if (operator === 'subtract') {
+        return subtract([firstNumber, secondNumber]);
+    } else if (operator === 'multiply') {
+        return multiply([firstNumber, secondNumber]);
+    } else if (operator === 'divide') {
+        return divide([firstNumber, secondNumber]);
+    }
+};
+
 function add(array) {
     function getSum(total, num) {
         return total + num;
